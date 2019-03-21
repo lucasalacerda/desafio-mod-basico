52f18ed1cb58ad2f4fbd9e4b1841cbf8a0cfef98
Lucas Alves de Lacerda <lucasalvlacerda@gmail.com>
2019-03-21 09:51:40 -0300

    change method using toggle instead of add and remove

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 function showMenuMobile() {
   const menuMobile = document.querySelector(".nav-menu-mobile");
 
-  if (!menuMobile.classList.contains("showMenuMobile")) {
-    menuMobile.classList.add("showMenuMobile");
-  } else if (menuMobile.classList.contains("showMenuMobile")) {
-    menuMobile.classList.remove("showMenuMobile");
-  }
+    menuMobile.classList.toggle("showMenuMobile");
+
 }
